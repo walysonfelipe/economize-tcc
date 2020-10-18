4f7b00a6f183a97d0fa5ccec9d128078f8843bc9
Waly <walysonfelipe25@gmail.com>
2020-10-18 16:01:06 -0300

    remove credenciais

--- __system__/functions/pagseguro/configuration.php
+++ __system__/functions/pagseguro/configuration.php
@@ -1,22 +1,23 @@
 ﻿<?php
     $sandbox = true; // DEFININDO SE ESTÁ EM PRODUÇÃO OU EM TESTE
 
+ # Adicionar suas credenciais do meio de pagamento PAGSEGURO
     if ($sandbox) { // EM FASE DE TESTE
-        define("EMAIL_PAGSEGURO", "aneru.contato@gmail.com");
-        define("TOKEN_PAGSEGURO", "9E40B586074F402CA860948FF7020DD6");
+        define("EMAIL_PAGSEGURO", "EMAIL");
+        define("TOKEN_PAGSEGURO", "TOKEN");
         define("URL_PAGSEGURO", "https://ws.sandbox.pagseguro.uol.com.br/v2/");
         define("SCRIPT_PAGSEGURO", "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js");
 
-        define("EMAIL_LOJA", "aneru.contato@gmail.com");
+        define("EMAIL_LOJA", "teste@siteteste.com.br");
         define("MOEDA_PAGAMENTO", "BRL");
         define("URL_NOTIFICATION", Project::baseUrlPhp() . "compra/extrato");
     } else { // EM PRODUÇÃO
-        define("EMAIL_PAGSEGURO", "aneru.contato@gmail.com");
-        define("TOKEN_PAGSEGURO", "9E40B586074F402CA860948FF7020DD6");
+        define("EMAIL_PAGSEGURO", "teste@siteteste.com.br");
+        define("TOKEN_PAGSEGURO", "TOKEN");
         define("URL_PAGSEGURO", "https://ws.pagseguro.uol.com.br/v2/");
         define("SCRIPT_PAGSEGURO", "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js");
         
-        define("EMAIL_LOJA", "aneru.contato@gmail.com");
+        define("EMAIL_LOJA", "teste@siteteste.com.br");
         define("MOEDA_PAGAMENTO", "BRL");
         define("URL_NOTIFICATION", Project::baseUrlPhp() . "compra/extrato");
     }
